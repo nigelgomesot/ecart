9c384b8d3e668a47a046d3a1b72a31a5521d15ae
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-03-10 12:47:28 -0600

    # auth WIP...

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 const crypto = require('crypto')
+const jsonwebtoken =  require('jsonwebtoken')
+// TODO: use env variable
+const secret = crypto.randomBytes(16).toString('hex')
 
 const UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, unique: true, required: [true, 'cannot be blank'], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
@@ -18,6 +21,26 @@ UserSchema.methods.validatePassword = function(password) {
   const incomingHash = crypto.pbkdf2Sync(password, this.salt, 1000, 512, 'sha512').toString('hex')
 
   return this.hash === incomingHash
+}
+
+UserSchema.methods.generateJWT = function() {
+  const today = new Date()
+  let exp = new Date(today)
+
+  exp.setDate(today.getDate() + 60)
+
+  return jsonwebtoken.sign({
+    id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000)
+  }, secret)
+}
+
+UserSchema.methods.toAuthJSON = function() {
+  return {
+    username: this.username,
+    token: this.generateJWT()
+  }
 }
 
 UserSchema.plugin(uniqueValidator, {message: 'is already taken'})
