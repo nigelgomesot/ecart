ae9cf96210fc5d511614803e8490b34713b451a6
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-03-15 09:55:14 -0500

    # auth WIP...

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const auth = require('../auth')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 
+// user signup
 router.post('/users', (req, res, next) => {
   const user = new User()
   user.username = req.body.user.username
@@ -14,5 +15,28 @@ router.post('/users', (req, res, next) => {
     return res.json({user: user.toAuthJSON()})
   }).catch(next)
 })
+
+// user signin
+router.post('/users/login', (req, res, next) => {
+  if (!req.body.user.email)
+    return res.sendStatus(422).json({errors: {email: 'cannot be blank'}})
+
+  if (!req.body.user.password)
+    return res.sendStatus(422).json({errors: {password: 'cannot be blank'}})
+
+  passport.authenticate('local', {session: false}, (err, user, info) => {
+    if (err)
+      return next(err)
+
+    if (user) {
+      user.token = user.generateJWT()
+
+      return res.json({user: user.toAuthJSON()})
+    } else {
+      return res.status(422).json(info)
+    }
+  })(req, res, next)
+})
+
 
 module.exports = router
