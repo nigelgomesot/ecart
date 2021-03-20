dde9b204c43b74c3dd5ec243e3ff8411b5cea767
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2021-03-20 13:20:11 -0500

    # product WIP...

--- routes/api/products.js
+++ routes/api/products.js
@@ -6,6 +6,19 @@ const User = mongoose.model('User')
 const Product = mongoose.model('Product')
 const auth = require('../auth')
 
+router.param('product', (req, res, next, slug) => {
+  // TODO: add CreatedBy
+  Product.findOne({slug: slug})
+    .then(product => {
+      if (!product)
+        return res.sendStatus(404)
+
+        req.product = product
+
+        return next()
+    }).catch(next)
+})
+
 // create product
 router.post('/', auth.required, (req, res, next) => {
   User.findById(req.payload.id).then(user => {
@@ -19,6 +32,19 @@ router.post('/', auth.required, (req, res, next) => {
     return product.save().then(() => {
       return res.json({product: product.toJSONFor(user)})
     })
+  }).catch(next)
+})
+
+// get product
+router.get('/:product', auth.optional, (req, res, next) => {
+  Promise.all([
+    req.payload ? User.findById(req.payload.id) : null,
+    req.product
+  ]).then(results => {
+    const user = results[0],
+          product = results[1]
+
+    return res.json({product: product.toJSONFor(user)})
   }).catch(next)
 })
 
