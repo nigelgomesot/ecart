4079d2e09a2da6caf86a5d0124aabbb5c4cbde95
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-05-11 13:20:26 -0500

    # test WIP...

--- spec/routes/api/products_spec.js
+++ spec/routes/api/products_spec.js
@@ -7,13 +7,48 @@ chai.use(chaiHttp)
 const app = require('../../../app')
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
+const User = mongoose.model('User')
 
 describe('Products', () => {
+  let user
+
   beforeEach(done => {
     Product.deleteMany().then(() => done())
   })
 
+  beforeEach(done => {
+    const u = new User()
+    u.uname = 'test'
+    u.email = 'test@test.com'
+    u.setPassword('123456')
+
+    u.save().then(() => {
+      user = u
+
+      done()
+    })
+  })
+
   // PENDING: Create a product
+  describe('POST /api/products', () => {
+    it('should create a product', done => {
+      let product = { "product":
+        {
+          "sku": "RN14",
+          "title":"ring 14",
+          "description":"a silver ring",
+          "categoryList":["rings"], "price": 80
+        }
+      }
+
+      chai.request(app)
+        .post('/api/products')
+        .send(product)
+        .end((err, response) => {
+          response.should.have.status(201)
+        })
+    })
+  })
 
   // TODO: fetch specific product
   // describe('GET /api/products', () => {
